a2290c6b9820423c4d43190cf8c35d35b8bdfc0d
Johan <jwanglof@gmail.com>
2013-03-03 21:33:40 +0100

    EEEVERYTHING

--- worldroc.py
+++ worldroc.py
@@ -16,6 +16,14 @@ def index():
     childPictures = io.loadPictures(childDir)
     return render_template('home.html', title='Home', childDir=childDir, childPictures=childPictures)
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact us')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
